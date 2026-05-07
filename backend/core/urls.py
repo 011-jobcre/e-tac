ba889bce42c9212395ff6ed74f95b34cee06043f
JCHD <jchd@JCHDnoMacBook-Air.local>
2026-05-07 18:02:16 +0900

    commit tam rollback duoc

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-from .views import HomeView, AboutView, ServicesView, ContactView
+from .views import HomeView, AboutView, ServicesView, ContactView, InquiryView, GroupView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('rosetta/', include('rosetta.urls')),
+    path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+    path("rosetta/", include("rosetta.urls")),
 ]
 
 urlpatterns += i18n_patterns(
-    path('', HomeView.as_view(), name='home'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('services/', ServicesView.as_view(), name='services'),
-    path('contact/', ContactView.as_view(), name='contact'),
+    path("", HomeView.as_view(), name="home"),
+    path("about/", AboutView.as_view(), name="about"),
+    path("services/", ServicesView.as_view(), name="services"),
+    path("contact/", ContactView.as_view(), name="contact"),
+    path("inquiry/", InquiryView.as_view(), name="inquiry"),
+    path("group/", GroupView.as_view(), name="group"),
 )
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,13 +1,26 @@
 from django.views.generic import TemplateView
 
+
 class HomeView(TemplateView):
     template_name = "homepage.html"
+    # template_name = "etac_page.html"
+
 
 class AboutView(TemplateView):
-    template_name = "about_us.html"
+    template_name = "about.html"
+
 
 class ServicesView(TemplateView):
     template_name = "services.html"
 
+
 class ContactView(TemplateView):
-    template_name = "contact_us.html"
+    template_name = "contact.html"
+
+
+class InquiryView(TemplateView):
+    template_name = "inquiry.html"
+
+
+class GroupView(TemplateView):
+    template_name = "group.html"
